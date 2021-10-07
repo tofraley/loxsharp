ad9a0d2695e9638fca0252b6c2012983bbfa973a
Taylor Fraley <tfraley@clearent.com>
2021-10-07 09:10:38 -0400

    Add project files.

---
+++ loxsharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace loxsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
