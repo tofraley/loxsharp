dae0dc6bed08d736c6ed405c98f88d4c4e767068
Taylor Fraley <tfraley@clearent.com>
2021-10-08 15:05:50 -0400

    move initial logic to Lox class

---
+++ loxsharp/Lox.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace loxsharp
+{
+    public class Lox
+    {
+        Boolean _hadError = false;
+        int _sysExit = 0;
+
+        public int GetSysExit() => _sysExit;
+
+        public void RunPrompt()
+        {
+            for (;;)
+            {
+                Console.Write("> ");
+                String line = Console.ReadLine();
+                if (line == null) break;
+                Run(line);
+                _hadError = false;
+            }
+        }
+
+        private void Run(string source)
+        {
+            Scanner scanner = new Scanner(source);
+            List<Token> tokens = scanner.ScanTokens();
+
+            // For now, just print the tokens.
+            foreach (Token token in tokens)
+            {
+                Console.WriteLine(token);
+            }
+        }
+
+        public void RunFile(string path)
+        {
+            String text = File.ReadAllText(Path.GetFullPath(path));
+            Run(text); 
+            if (_hadError) _sysExit = 65;
+        }
+
+        private void Error(int line, String message)
+        {
+            Report(line, "", message);
+        }
+
+        private void Report(int line, String where,
+                                   String message)
+        {
+            Console.Error.WriteLine(
+                "[line " + line + "] Error" + where + ": " + message);
+            _hadError = true;
+        }
+    }
+}
--- loxsharp/Program.cs
+++ loxsharp/Program.cs
@@ -6,68 +6,24 @@ namespace loxsharp
 {
     class Program
     {
-        static Boolean hadError = false;
-        static int sysExit = 0;
         static int Main(string[] args)
         {
+            Lox lox = new Lox();
             if (args.Length > 1)
             {
                 Console.WriteLine("Usage: lox [script]");
             }
             else if (args.Length == 1)
             {
-                RunFile(args[0]);
+                lox.RunFile(args[0]);
             }
             else
             {
-                RunPrompt();
+                lox.RunPrompt();
             }
 
-            return sysExit;
+            return lox.GetSysExit();
         }
 
-        private static void RunPrompt()
-        {
-            for (;;)
-            {
-                Console.Write("> ");
-                String line = Console.ReadLine();
-                if (line == null) break;
-                Run(line);
-                hadError = false;
-            }
-        }
-
-        private static void Run(string source)
-        {
-            Scanner scanner = new Scanner(source);
-            List<Token> tokens = scanner.ScanTokens();
-
-            // For now, just print the tokens.
-            foreach (Token token in tokens)
-            {
-                Console.WriteLine(token);
-            }
-        }
-
-        private static void RunFile(string path)
-        {
-            String text = File.ReadAllText(Path.GetFullPath(path));
-            Run(text); 
-            if (hadError) sysExit = 65;
-        }
-
-        static void Error(int line, String message)
-        {
-            Report(line, "", message);
-        }
-
-        private static void Report(int line, String where,
-                                   String message)
-        {
-            Console.Error.WriteLine(
-                "[line " + line + "] Error" + where + ": " + message);
-            hadError = true;
-        }
     }
 }
