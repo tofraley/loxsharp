5ea65ef80701047e8dfa2a8b0b408040c65ad415
Taylor Fraley <tfraley@clearent.com>
2021-10-08 15:33:24 -0400

    init Token class

--- loxsharp/Token.cs
+++ loxsharp/Token.cs
@@ -1,6 +1,25 @@
-﻿namespace loxsharp
+﻿using System;
+
+namespace loxsharp
 {
     internal class Token
     {
+        readonly TokenType Type;
+        readonly String Lexeme;
+        readonly Object Literal;
+        readonly int Line;
+
+        Token(TokenType type, String lexeme, Object literal, int line)
+        {
+            this.Type = type;
+            this.Lexeme = lexeme;
+            this.Literal = literal;
+            this.Line = line;
+        }
+
+        public override String ToString()
+        {
+            return Type + " " + Lexeme + " " + Literal;
+        }
     }
 }
