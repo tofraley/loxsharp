c10cf002ca5a6465b14db4c970a188d418149dbe
Taylor Fraley <tfraley@clearent.com>
2021-10-10 12:37:30 -0400

    create AstGenerator project

---
+++ GenerateAst/AstGenerator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+using RazorEngine;
+using RazorEngine.Templating;
+using HandlebarsDotNet;
+
+namespace loxsharp.tool
+{
+
+    class Field
+    {
+        public String Name { get; set; }
+        public String Type { get; set; }
+    }
+
+    class Expression
+    {
+        public String ClassName { get; set; }
+        public IEnumerable<Field> Fields { get; set; }
+    }
+
+    public class AstGenerator
+    {
+        public void Run(String[] args)
+        {
+            if (args.Length != 3) {
+              Console.Error.WriteLine("Usage: generate_ast <types path> <template path> <output path>");
+              Environment.Exit(64);
+            }
+            String typesDir = args[0];
+            String templateDir = args[1];
+            String outputDir = args[2];
+
+            DefineAst(typesDir, templateDir, outputDir);
+        }
+
+        private void DefineAst(String typesDir, String templateDir, String outputDir)
+        {
+            var yml = File.ReadAllText(typesDir);
+            var template = Handlebars.Compile(File.ReadAllText(templateDir));
+            var deserializer = new DeserializerBuilder()
+                .Build();
+
+            var types = deserializer.Deserialize<List<Expression>>(yml);
+            var result = template(types);
+            File.WriteAllText(outputDir, result);
+        }
+    }
+}
---
+++ GenerateAst/Program.cs
@@ -0,0 +1,16 @@
+﻿using loxsharp.tool;
+using System;
+using System.IO;
+
+namespace GenerateAst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AstGenerator gen = new AstGenerator();
+            gen.Run(new string[] { "Types.yml", "Expr.hbs", "C:\\clearent\\projects\\loxsharp\\loxsharp\\Expr.cs" });
+            Console.WriteLine("Done");
+        }
+    }
+}
