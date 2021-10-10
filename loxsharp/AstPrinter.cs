748cf7b782869cb7c1210e30c5f1e5e2875b06bb
Taylor Fraley <tfraley@clearent.com>
2021-10-10 14:32:35 -0400

    create AstPrinter class...might delete

---
+++ loxsharp/AstPrinter.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace loxsharp
+{
+    public class AstPrinter : Expr.Visitor<String>
+    {
+        public String Print(Expr expr)
+        {
+            return expr.Accept(this);
+        }
+
+        public String VisitBinaryExpr(Expr.Binary expr)
+        {
+            return Parenthesize(expr.Operator.Lexeme,
+                                new List<Expr> { expr.Left, expr.Right });
+        }
+
+        public String VisitGroupingExpr(Expr.Grouping expr)
+        {
+            return Parenthesize("group", new List<Expr> { expr.Expression });
+        }
+
+        
+        public String VisitLiteralExpr(Expr.Literal expr)
+        {
+            if (expr.Value == null) return "nil";
+            return expr.Value.ToString();
+        }
+
+        
+        public String VisitUnaryExpr(Expr.Unary expr)
+        {
+            return Parenthesize(expr.Operator.Lexeme, new List<Expr> { expr.Right });
+        }
+
+        private String Parenthesize(String name, IEnumerable<Expr> exprs)
+        {
+            StringBuilder builder = new StringBuilder();
+
+            builder.Append("(").Append(name);
+            foreach (Expr expr in exprs)
+            {
+                builder.Append(" ");
+                builder.Append(expr.Accept(this));
+            }
+            builder.Append(")");
+
+            return builder.ToString();
+        }
+    }
+}
