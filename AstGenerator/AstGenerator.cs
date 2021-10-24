c5ce4c0009a0aa948edeb028605abbfe6f257608
Taylor Fraley <taylor.o.fraley@protonmail.com>
2021-10-24 13:18:29 -0400

    add ability to generate multiple abstract classes to AstGenerator

--- AstGenerator/AstGenerator.cs
+++ AstGenerator/AstGenerator.cs
@@ -7,16 +7,28 @@ using HandlebarsDotNet;
 namespace loxsharp.tool
 {
 
+    class AbstractClassCollection
+    {
+        public IEnumerable<AbstractClass> AbstractClasses { get; set; }
+    }
+
+    class AbstractClass
+    {
+        public String AbstractClassName { get; set; }
+        public String AbstractParam => AbstractClassName.ToLower();
+        public IEnumerable<ImplClass> ImplClasses { get; set; }
+    }
+
+    class ImplClass
+    {
+        public String ClassName { get; set; }
+        public IEnumerable<Field> Fields { get; set; }
+    }
+
     class Field
     {
         public String Name { get; set; }
         public String Type { get; set; }
-    }
-
-    class Expression
-    {
-        public String ClassName { get; set; }
-        public IEnumerable<Field> Fields { get; set; }
     }
 
     public class AstGenerator
@@ -27,17 +39,23 @@ namespace loxsharp.tool
             String templateDir = args[1];
             String outputDir = args[2];
 
+            Console.WriteLine($"{typesDir}, {templateDir}, {outputDir}");
             DefineAst(typesDir, templateDir, outputDir);
         }
 
         private void DefineAst(String typesDir, String templateDir, String outputDir)
         {
+            Console.WriteLine($"Reading {typesDir}");
             var yml = File.ReadAllText(typesDir);
+            Console.WriteLine($"Compiling {templateDir}");
             var template = Handlebars.Compile(File.ReadAllText(templateDir));
+            Console.WriteLine("Creating deserializer");
             var deserializer = new DeserializerBuilder()
                 .Build();
 
-            var types = deserializer.Deserialize<List<Expression>>(yml);
+            Console.WriteLine("Deserializing...");
+            var types = deserializer.Deserialize<List<AbstractClass>>(yml);
+            Console.WriteLine("Done deserializing...");
             var result = template(types);
             File.WriteAllText(outputDir, result);
         }
